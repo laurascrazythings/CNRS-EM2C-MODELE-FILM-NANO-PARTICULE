7fef9b1e66e0a7acf2c4eea86f93b43d35ef84b4
laurascrazythings <92514354+laurascrazythings@users.noreply.github.com>
2025-12-05 15:16:55 +0100

    update test mpi

--- CODE_2D_PARALLEL/Collision_model_parallel.py
+++ CODE_2D_PARALLEL/Collision_model_parallel.py
@@ -1,29 +1,40 @@
-#I am now going to try to recode what was done in the source code for collision_model using the mpi method
+#I am wanting to code a mpi code that can send one particule from one proc to another
 from mpi4py import MPI
 import numpy as np
-import matplotlib.pyplot as plt
-from numba import jit
-from matplotlib.animation import FuncAnimation
-from scipy.spatial import cKDTree
-from scipy import interpolate
-import datetime as dtm
-import csv
 
-import Calculation
+comm = MPI.COMM_WORLD #INIT the mpi
+my_rank = comm.Get_rank()
+my_size = comm.Get_size()
 
-### Initialization ###
+a = 0.0
+b = 1.0
+n = 1024
+dest = 0
+total = -1.0
+h = (b - a) / n
 
-simulation_time = 1 # Total simulation time
+local_n = n // my_size
+local_a = a + my_rank * local_n * h;
+local_b = local_a + local_n * h 
+# build local grid and values
+x_local = np.linspace(local_a, local_b, local_n + 1 )
+y_local = (x_local)**2
 
-# Runtime calculation
-beginning_date_and_time = dtm.datetime.now()
-
-
-# Animation saving
-SAVE_ANIMATION = False # Constant to control animation saving
-
-# Position saving
-SAVE_POSITION = True # Constant to control position saving in a csv
-
-# Print kinetic energy
-PRINT_KINETIC_ENERGY = False # Constant to control kinetic energy printing
+# local integral
+integral = np.trapezoid(y_local, dx=h)
+#add up the integrals
+if my_rank == 0:
+    total = integral
+    for source in range (1,my_size):
+        integral = comm.recv(source = source)
+        print ("Processor", my_rank, "<-", source, ",", integral, "\n")
+        total = total + integral 
+else:
+    print("Processor", my_rank, "->", dest, ",", integral, "\n")
+    comm.send(integral, dest = 0)
+    
+#print the result
+if(my_rank == 0) :
+    print("With n = ", n, "Integral from ", a, "to ", b, "is equal to ", total)
+    
+MPI.Finalize
---
+++ CODE_2D_PARALLEL/one_particule_one_mesh.py
@@ -0,0 +1,54 @@
+#imports 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+#Time 
+T = 5
+dt = 0.0005
+Nt = int(T / dt)
+
+#init envi
+L = np.array([4, 20]) # Lx = L [0] width, Ly = [1] height => Mesh size (µm)
+Np = 1 #numbr of particules
+radius_diameter = 10**(-2) #Radius of a particule (µm)
+
+#init position and velocity
+XY_start = np.array([0.2, L[1]/2])
+Vp = np.array([0.7, 0])# in (µm/s)
+
+Vp_dt = Vp * dt
+
+Nt_index = np.arange(Nt)
+XY_saved = np.array([XY_start[0] + Vp_dt[0]*Nt_index, XY_start [1] + Vp_dt[1]*Nt_index])
+# print(XY_saved[0,:5])
+
+# plot
+# --- Figure and initial scatter ---
+fig, ax = plt.subplots()
+
+# Set domain to your physical box
+ax.set_xlim(0, L[0])
+ax.set_ylim(0, L[1])
+ax.set_xlabel("x (µm)")
+ax.set_ylabel("y (µm)")
+
+# One particle: start with empty data
+scat = ax.scatter([], [], s=50)
+
+# (Optional) nice aspect ratio
+ax.set_aspect("equal", adjustable="box")
+
+def update(frame):
+    position_x = XY_saved [0, frame]
+    position_y = XY_saved [1, frame]
+    # set_offsets expects an array of shape (n_points, 2)
+    scat.set_offsets([[position_x, position_y]])
+    current_time = frame * dt
+    ax.set_title(f"Particle animation (t = {current_time:.3f} s)")
+    return scat,
+
+# --- Create animation ---
+ani = FuncAnimation(fig, update, frames=Nt,interval=10,blit=True)
+
+plt.show()
